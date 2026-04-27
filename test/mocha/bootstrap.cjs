30581cc652cc9b7ed0f692fdc64bb446bc4be1ac
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-04-27 22:54:02 +0100

    Upgrade chai and simon dependencies (#21)

--- test/mocha/bootstrap.cjs
+++ test/mocha/bootstrap.cjs
@@ -2,8 +2,8 @@
 
 const chai = require('chai');
 
-chai.use(require('chai-as-promised'));
-chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised').default);
+chai.use(require('sinon-chai').default);
 
 process.env.SLS_DEPRECATION_NOTIFICATION_MODE ??= 'error';
 process.env.SLS_TELEMETRY_DISABLED = '1';
