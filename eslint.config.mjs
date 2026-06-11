466ab0f1c19c6d46c0b3d897bea3c65f04a6ff7b
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-11 01:57:38 +0100

    Upgrade eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,36 @@
-'use strict';
-
-const js = require('@eslint/js');
-const globals = require('globals');
-const importX = require('eslint-plugin-import-x');
-const n = require('eslint-plugin-n');
-const eslintConfigPrettier = require('eslint-config-prettier/flat');
+import js from '@eslint/js';
+import globals from 'globals';
+import { flatConfigs as importXFlatConfigs } from 'eslint-plugin-import-x';
+import n from 'eslint-plugin-n';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const devDependencyFiles = [
   '**/*.test.js',
   '**/scripts/**',
   '**/test/**',
   '**/tests/**',
-  'eslint.config.js',
+  'eslint.config.mjs',
   'prettier.config.js',
 ];
 
-module.exports = [
+export default defineConfig([
   {
+    name: 'compose/linter-options',
     linterOptions: {
       reportUnusedDisableDirectives: 'warn',
     },
   },
-  {
-    ignores: ['**/.*'],
-  },
+  globalIgnores(['**/.*']),
   js.configs.recommended,
   n.configs['flat/recommended-script'],
-  importX.flatConfigs.recommended,
+  importXFlatConfigs.recommended,
   eslintConfigPrettier,
   {
+    name: 'compose/javascript',
     files: ['**/*.{cjs,js,mjs}', 'bin/serverless-compose'],
     languageOptions: {
-      ecmaVersion: 2023,
+      ecmaVersion: 2025,
       sourceType: 'commonjs',
       globals: {
         ...globals.node,
@@ -52,28 +51,36 @@ module.exports = [
           caughtErrors: 'all',
         },
       ],
+      'n/no-extraneous-import': 'off',
       'n/no-unsupported-features/node-builtins': ['error', { allowExperimental: true }],
       'n/no-extraneous-require': 'off',
+      'n/no-missing-import': 'off',
+      'n/no-unpublished-import': 'off',
       'n/no-unpublished-require': 'off',
       'n/no-missing-require': 'off',
       'n/no-process-exit': 'off',
       'n/no-deprecated-api': 'off',
       'n/hashbang': 'off',
+      'n/no-unpublished-bin': 'error',
     },
   },
   {
+    name: 'compose/published-files',
     files: ['bin/serverless-compose', 'components/**/*.js', 'src/**/*.js'],
     rules: {
+      'n/no-unpublished-import': 'error',
       'n/no-unpublished-require': 'error',
     },
   },
   {
+    name: 'compose/modules',
     files: ['**/*.mjs'],
     languageOptions: {
       sourceType: 'module',
     },
   },
   {
+    name: 'compose/tests',
     files: ['**/*.test.js', '**/test/**'],
     languageOptions: {
       globals: globals.mocha,
@@ -82,4 +89,4 @@ module.exports = [
       'no-unused-expressions': 'off',
     },
   },
-];
+]);
